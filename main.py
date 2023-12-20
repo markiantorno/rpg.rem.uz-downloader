58af4b401d0f3ae399b010d2e768292a44999e0b
Mark Iantorno <markiantorno@gmail.com>
2023-12-20 11:19:01 -0500

    Update main.py
    Updating script to check if the file already exists and skip if it does.

--- main.py
+++ main.py
@@ -18,10 +18,14 @@ def parse_url(directory, url):
 
 def download_pdf_from_url(directory, url, filename):
     request = requests.get(url)
-    print(f'Downloading from {url} into {directory}...')
-    with open(directory + '/' + filename, mode='wb') as local_file:
-        local_file.write(request.content)
-
+    file_path = directory + '/' + filename
+    if os.path.exists(file_path):
+    	print(f'Skipping {file_path}, as it already exists...')
+    else:
+    	print(f'Downloading from {url} into {directory}...')
+    	with open(file_path, mode='wb') as local_file:
+        	local_file.write(request.content)
+            
 
 target_url = "https://the-eye.eu/public/Books/rpg.rem.uz/Dungeons%20%26%20Dragons/"
 target_dir = 'Dungeons & Dragons'
